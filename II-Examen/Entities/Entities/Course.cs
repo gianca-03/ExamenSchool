182b5fb1ce0236725e4f8a27a6e623ed4789c2df
gianca-03 <gianca.ruiz@hotmail.com>
2023-08-15 18:32:41 -0600

    DB mapeo

---
+++ II-Examen/Entities/Entities/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities
+{
+    public partial class Course
+    {
+        public Course()
+        {
+            StudentGrades = new HashSet<StudentGrade>();
+            People = new HashSet<Person>();
+        }
+
+        public int CourseId { get; set; }
+        public string Title { get; set; } = null!;
+        public int Credits { get; set; }
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; } = null!;
+        public virtual OnlineCourse? OnlineCourse { get; set; }
+        public virtual OnsiteCourse? OnsiteCourse { get; set; }
+        public virtual ICollection<StudentGrade> StudentGrades { get; set; }
+
+        public virtual ICollection<Person> People { get; set; }
+    }
+}
---
+++ II-Examen/Entities/Entities/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            StudentGrades = new HashSet<StudentGrade>();
+            Courses = new HashSet<Course>();
+        }
+
+        public int PersonId { get; set; }
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public DateTime? HireDate { get; set; }
+        public DateTime? EnrollmentDate { get; set; }
+        public string Discriminator { get; set; } = null!;
+
+        public virtual OfficeAssignment? OfficeAssignment { get; set; }
+        public virtual ICollection<StudentGrade> StudentGrades { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
